4a992401507a05e50e009483245162cb45dc1fe9
katushka211 <113642729+katushka211@users.noreply.github.com>
2023-06-18 08:29:53 +0000

    Deploying to gh-pages from @ katushka211/goit-react-hw-08-phonebook@9674795230f26cbcdaa6725c2b63876b1b641188 🚀

---
+++ static/js/805.82167d0a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[805],{9805:function(e,n,o){o.r(n);var t=o(1087),s=o(184);n.default=function(){return(0,s.jsxs)("div",{children:[(0,s.jsx)("p",{className:"not-found-text",children:"Page not found"}),(0,s.jsx)(t.rU,{className:"link-details",to:"/",children:"Back to Home"})]})}}}]);
+//# sourceMappingURL=805.82167d0a.chunk.js.map
