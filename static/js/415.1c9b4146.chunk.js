a8b4d775119c390df959aca2e82653a4eee4c6e1
katushka211 <113642729+katushka211@users.noreply.github.com>
2023-06-18 21:35:08 +0000

    Deploying to gh-pages from @ katushka211/goit-react-hw-08-phonebook@aa5632970301c22a9fc35a064523aa15f1f824ff 🚀

--- static/js/415.1c9b4146.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,n,o){o.r(n);var r=o(184);n.default=function(){return(0,r.jsx)("div",{children:(0,r.jsxs)("h1",{children:["Phonebook manager welcome page"," ",(0,r.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
-//# sourceMappingURL=415.1c9b4146.chunk.js.map
