af852bcd59b1d205aa85992607d5d6c87be5dd16
Kateryna Verveda <katushka211@gmail.com>
2023-10-12 17:39:10 +0200

    add validation for form

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,83 +1,106 @@
 import React from 'react';
 import { TextField, Button, Grid, Box } from '@mui/material';
+import { Formik, Form, ErrorMessage, Field } from 'formik';
 
 import { useDispatch } from 'react-redux';
 import { register } from '../../redux/auth/operation';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .required('Name is required')
+    .min(3, 'Name should be at least 3 characters')
+    .max(30, 'Name should not exceed 30 characters'),
+  email: Yup.string()
+    .required('Email is required')
+    .email('Invalid email address'),
+  password: Yup.string()
+    .required('Password is required')
+    .min(6, 'Password should be at least 6 characters')
+    .test(
+      'no-password',
+      'Password cannot contain the word "password"',
+      value => !value.toLowerCase().includes('password')
+    ),
+});
 
 export const RegisterForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      register({
-        name: form.elements.name.value,
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
+  const handleSubmit = (values, { resetForm }) => {
+    dispatch(register(values));
+    resetForm();
   };
 
   return (
-    <Box
-      component="form"
+    <Formik
+      initialValues={{ name: '', email: '', password: '' }}
+      validationSchema={validationSchema}
       onSubmit={handleSubmit}
-      autoComplete="off"
-      sx={{
-        margin: '20px auto',
-        width: 500,
-        justifyContent: 'center',
-        padding: '10',
-      }}
     >
-      <Grid container spacing={2} justifyContent="center">
-        <Grid item xs={12}>
-          <TextField
-            label="Username"
-            variant="outlined"
-            fullWidth
-            required
-            name="name"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Email"
-            variant="outlined"
-            fullWidth
-            required
-            name="email"
-            type="email"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Password"
-            variant="outlined"
-            fullWidth
-            required
-            name="password"
-            type="password"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button
-            type="submit"
-            variant="contained"
-            fullWidth
-            sx={{
-              backgroundColor: '#38d2d2',
-              '&:hover': {
-                backgroundColor: 'rgb(41, 132, 132)',
-              },
-            }}
-          >
-            Register
-          </Button>
-        </Grid>
-      </Grid>
-    </Box>
+      <Box
+        sx={{
+          margin: '20px auto',
+          width: 500,
+          justifyContent: 'center',
+          padding: '10',
+        }}
+      >
+        <Form>
+          <Grid container spacing={2} justifyContent="center">
+            <Grid item xs={12}>
+              <Field
+                as={TextField}
+                label="Username"
+                variant="outlined"
+                fullWidth
+                required
+                name="name"
+              />
+              <ErrorMessage name="name" component="div" className="error" />
+            </Grid>
+            <Grid item xs={12}>
+              <Field
+                as={TextField}
+                label="Email"
+                variant="outlined"
+                fullWidth
+                required
+                name="email"
+                type="email"
+              />
+              <ErrorMessage name="email" component="div" className="error" />
+            </Grid>
+            <Grid item xs={12}>
+              <Field
+                as={TextField}
+                label="Password"
+                variant="outlined"
+                fullWidth
+                required
+                name="password"
+                type="password"
+              />
+              <ErrorMessage name="password" component="div" className="error" />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                variant="contained"
+                fullWidth
+                sx={{
+                  backgroundColor: '#38d2d2',
+                  '&:hover': {
+                    backgroundColor: 'rgb(41, 132, 132)',
+                  },
+                }}
+              >
+                Register
+              </Button>
+            </Grid>
+          </Grid>
+        </Form>
+      </Box>
+    </Formik>
   );
 };
--- src/redux/auth/operation.js
+++ src/redux/auth/operation.js
@@ -65,7 +65,7 @@ export const refreshUser = createAsyncThunk(
     }
     try {
       setAuthHeader(persistedToken);
-      const response = await axios.get('/users/me');
+      const response = await axios.get('/users/current');
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
