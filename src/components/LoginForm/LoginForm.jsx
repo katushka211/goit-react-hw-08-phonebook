6d12698fe6bab54ef7bc67a00570b140fbf31aec
Kateryna Verveda <katushka211@gmail.com>
2023-10-13 14:01:04 +0200

    update login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,70 +1,156 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import { useDispatch } from 'react-redux';
 import { logIn } from '../../redux/auth/operation';
-import { TextField, Button, Grid, Box } from '@mui/material';
+import {
+  TextField,
+  Button,
+  Grid,
+  Box,
+  Typography,
+  Avatar,
+  Link,
+} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .required('Email is required')
+    .email('Invalid email address'),
+  password: Yup.string()
+    .required('Password is required')
+    .min(7, 'Password should be at least 7 characters')
+    .test(
+      'no-password',
+      'Password cannot contain the word "password"',
+      value => !value.toLowerCase().includes('password')
+    ),
+});
+
 export const LoginForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      logIn({
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
+  const handleSubmit = (values, { resetForm }) => {
+    dispatch(logIn(values));
+    resetForm();
   };
 
   return (
-    <Box
-      component="form"
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validationSchema={validationSchema}
       onSubmit={handleSubmit}
-      autoComplete="off"
-      sx={{
-        margin: '20px auto',
-        width: 500,
-        justifyContent: 'center',
-        padding: '10',
-      }}
     >
-      <Grid container spacing={2} justifyContent="center">
-        <Grid item xs={12}>
-          <TextField
-            label="Email"
-            variant="outlined"
-            fullWidth
-            required
-            name="email"
-            type="email"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Password"
-            variant="outlined"
-            fullWidth
-            required
-            name="password"
-            type="password"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button
-            type="submit"
-            variant="contained"
-            fullWidth
+      <Form>
+        <Box
+          sx={{
+            backgroundColor: 'rgba(248, 248, 248, 0.188)',
+            minHeight: '100%',
+          }}
+        >
+          <Box
             sx={{
-              backgroundColor: '#38d2d2',
-              '&:hover': {
-                backgroundColor: 'rgb(41, 132, 132)',
-              },
+              margin: '0 auto',
+              width: 500,
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              padding: '10',
             }}
           >
-            LogIn
-          </Button>
-        </Grid>
-      </Grid>
-    </Box>
+            <Form>
+              <Box
+                sx={{
+                  marginTop: 8,
+                  marginBottom: 3,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Avatar sx={{ m: 1, bgcolor: 'rgb(41, 132, 132)' }}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h2" variant="h5">
+                  Log In
+                </Typography>
+              </Box>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item xs={12}>
+                  <Field
+                    as={TextField}
+                    label="Email"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    name="email"
+                    type="email"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="error"
+                    style={{ color: 'red', fontSize: '14px' }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    as={TextField}
+                    label="Password"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    name="password"
+                    type="password"
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="error"
+                    style={{ color: 'red', fontSize: '14px' }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    fullWidth
+                    sx={{
+                      backgroundColor: '#38d2d2',
+                      marginBottom: '20px',
+                      '&:hover': {
+                        backgroundColor: 'rgb(41, 132, 132)',
+                      },
+                    }}
+                  >
+                    Log In
+                  </Button>
+                  <Box
+                    display={'flex'}
+                    justifyContent={'center'}
+                    gap={'5px'}
+                    alignItems={'center'}
+                  >
+                    <Typography component={'span'}>
+                      Don't have an account?
+                    </Typography>
+                    <Link
+                      href="/goit-react-hw-08-phonebook/register"
+                      variant="body2"
+                      color={'rgb(41, 132, 132)'}
+                      fontSize={'16px'}
+                    >
+                      Sign Up
+                    </Link>
+                  </Box>
+                </Grid>
+              </Grid>
+            </Form>
+          </Box>
+        </Box>
+      </Form>
+    </Formik>
   );
 };
